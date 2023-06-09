8cd26144683416daf814c090c5f5ae4550da67f3
isaharon <isabharon@gmail.com>
2023-06-09 15:13:34 +0800

    Update keymaps
    * Remove unused keymap

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,4 +1,3 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- Yank to "+ register used for the system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
